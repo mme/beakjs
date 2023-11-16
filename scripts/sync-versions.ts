5d9d50efdf1697f1bd6ad0b62892b9cb8dadf17e
Markus Ecker <markus.ecker@gmail.com>
2023-11-16 14:58:50 +0100

    Bump version

--- scripts/sync-versions.ts
+++ scripts/sync-versions.ts
@@ -5,8 +5,8 @@ const versionFilePath = "./VERSION";
 const rootPackageJsonPath = "./package.json";
 const otherPackageJsonPaths = [
   rootPackageJsonPath,
-  "./src/core/package.json",
-  "./src/react/package.json",
+  "./packages/core/package.json",
+  "./packages/react/package.json",
 ];
 
 // This function reads the VERSION file and returns the version string
